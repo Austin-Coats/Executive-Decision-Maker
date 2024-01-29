0626dd366f24b265d3c5bfbc6255d5ebf5fc1166
Austin <Austin@AUSTIN-WORKSTAT>
2024-01-29 10:56:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+for (; ; )
+{
+    Console.WriteLine("What are you looking to do?");
+    string decision = Console.ReadLine();
+
+    Random rnd = new Random();
+    int numb = rnd.Next(2);
+
+    if (numb == 0)
+    {
+        Console.WriteLine("Yes, you should.\n");
+    }
+
+    else
+    {
+        Console.WriteLine("No, you should not.\n");
+    }
+}
